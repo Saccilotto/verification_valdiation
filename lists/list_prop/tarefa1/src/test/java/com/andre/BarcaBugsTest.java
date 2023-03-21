664713641aad6273b885d457f83b104c94ff7df6
Saccilotto <andre.sacilotto@acad.pucrs.br>
2023-03-21 21:10:05 +0000

    new exercise

---
+++ lists/list_prop/tarefa1/src/test/java/com/andre/BarcaBugsTest.java
@@ -0,0 +1,178 @@
+package com.andre;
+
+import java.io.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BarcaBugsTest {
+    private BarcaBugs bb;
+
+    @BeforeEach
+    public void initialize() {}
+
+    @ParameterizedTest
+    @CsvSource({
+            "F62A01",
+            "F32A21",
+            "F2A21",
+            "F02A1",
+            "Z02A19",
+            "F03X19"
+    })
+    public void testaAssentoInvalido(String assento) {
+        bb = new BarcaBugs();
+
+        int rEsp = 0;
+        int rObs = bb.ocupaLugar(assento);
+        Assertions.assertEquals(rEsp, rObs);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "F10A18,0,3",
+            "F42A10,6,3",
+            "F35A01,13,3",
+            "F36A17,0,2",
+            "F47A16,0,2",
+            "F37A15,6,2"
+    })
+    public void testaOcupaLugar(String assento, int qtdadeFilasOcupar,int rEsp) {
+        bb = new BarcaBugs();
+
+        int j = 0;
+        int fileira = assento.substring(1, 3).charAt(0) == '0' ? Integer.parseInt(assento.substring(2, 3)) - 1: Integer.parseInt(assento.substring(1, 3)) - 1;
+        int nroAssento = assento.substring(4, 6).charAt(0) == '0' ? Integer.parseInt(assento.substring(5, 6)) - 1: Integer.parseInt(assento.substring(4, 6)) - 1;
+        String aux;
+        bb.ocupaLugar(assento);
+        while(j == qtdadeFilasOcupar) {
+            for(int i = nroAssento+1; i <= 20; i++) {
+                aux = assento.replaceFirst(Integer.toString(nroAssento), Integer.toString(i++));
+                bb.ocupaLugar(aux);
+                if(i == 20) {
+                    i = 1;
+                }
+            }
+            aux = assento.replaceFirst(Integer.toString(fileira), Integer.toString(j++));
+            j++;
+        }   
+    }
+
+    @ParameterizedTest
+    @CsvSource({  
+        "F01A01",  
+        "F20A20"
+    })
+    public void testaLimitesAssentos100(String assento) throws FileNotFoundException {
+        bb = new BarcaBugs();
+        
+        String line = "";
+        try {
+            String filePath = "99.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+            
+            while((line = reader.readLine()) != null) {
+                String token = line; 
+                bb.ocupaLugar(token);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        int rEsp = 3;
+        int rObs = bb.ocupaLugar(assento);
+        Assertions.assertEquals(rEsp, rObs);
+    }
+
+    @ParameterizedTest
+    @CsvSource({    
+        "F40A01",
+        "F60A20"
+    })
+    public void testaLimitesAssentos101(String assento) throws FileNotFoundException {
+        bb = new BarcaBugs();
+        
+        String line = "";
+        try {
+            String filePath = "100.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+            
+            while((line = reader.readLine()) != null) {
+                String token = line; 
+                bb.ocupaLugar(token);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        int rEsp = 3;
+        int rObs = bb.ocupaLugar(assento);
+        Assertions.assertEquals(rEsp, rObs);
+    }
+
+    @ParameterizedTest
+    @CsvSource({    
+        "F40A01",
+        "F60A20"
+    })
+    public void testaLimitesAssentos200(String assento) throws FileNotFoundException {
+        bb = new BarcaBugs();
+        
+        String line = "";
+        try {
+            String filePath = "199.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            while((line = reader.readLine()) != null) {
+                String token = line; 
+                bb.ocupaLugar(token);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int rEsp = 3;
+        int rObs = bb.ocupaLugar(assento);
+        Assertions.assertEquals(rEsp, rObs);
+    }
+
+    @ParameterizedTest
+    @CsvSource({   
+        "F01A01", 
+        "F20A20",
+        "F35A20",
+        "F40A01",
+        "F45A15",
+        "F60A20"
+    })
+    public void testaLimitesAssentos201(String assento) throws FileNotFoundException {
+        bb = new BarcaBugs();
+        
+        String line = "";
+        try {
+            String filePath = "200.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            while((line = reader.readLine()) != null) {
+                String token = line; 
+                bb.ocupaLugar(token);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int rEsp = 3;
+        int rObs = bb.ocupaLugar(assento);
+        Assertions.assertEquals(rEsp, rObs);
+    }
+
+}
---
+++ lists/list_prop/tarefa1/src/test/java/com/andre/RankingTeste.java
@@ -0,0 +1,107 @@
+package com.andre;
+
+import java.io.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RankingTeste {
+
+    @BeforeEach
+    public void initialize() {}
+
+    @ParameterizedTest
+    @CsvSource({
+            "Andre Fetuccini,-9999",
+            "Zicavirus,-12313",
+            "Manso,-1823812038123",
+            "Copstein,-5678",
+            "Famous,-90123941203",
+            "Andre Fetuccini,-9999",
+            "Zicavirus, 12313",
+            "Manso,-1823812038123",
+            "Copstein,-5678",
+            "Famous,-90123941203",
+            "Andre Fetuccini,0",
+            "Zicavirus,0",
+            "Manso,0",
+            "Copstein,0",
+            "Famous,0"
+    })
+    public void testaValorNegativoNuloRepetido(String name, int score) {
+        Record rec = new Record(name, score);
+        Ranking aux = new Ranking();
+        aux.add(rec);
+    }
+
+    @Test
+    public void testaPrimeiroElemento() throws FileNotFoundException {
+        String line = "";
+        String delimiter = ",";
+        Ranking aux = new Ranking();
+        try {
+            String filePath = "RetiradaElementos.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            while((line = reader.readLine()) != null) {
+                String[] token = line.split(delimiter); 
+                Record rec = new Record(token[0], Integer.parseInt(token[1]));
+                aux.add(rec);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assertions.assertEquals("Primeiro" , aux.bestScore().getName());
+    }
+    
+    @Test
+    public void testaUltimoElementoRetirada() throws FileNotFoundException {
+        String line = "";
+        String delimiter = ",";
+        Ranking aux = new Ranking();
+        try {
+            String filePath = "RetiradaElementos.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            while ((line = reader.readLine()) != null) {
+                String[] token = line.split(delimiter); 
+                Record rec = new Record(token[0], Integer.parseInt(token[1]));
+                aux.add(rec);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assertions.assertEquals("Ultimo" , aux.worstScore().getName());
+    } 
+
+    @Test
+    public void testaNumeroElementos() throws FileNotFoundException {
+        String line = "";
+        String delimiter = ",";
+        Ranking aux = new Ranking();
+        try {
+            String filePath = "RetiradaElementos.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            while ((line = reader.readLine()) != null) {
+                String[] token = line.split(delimiter); 
+                Record rec = new Record(token[0], Integer.parseInt(token[1]));
+                aux.add(rec);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        Assertions.assertEquals(20 , aux.numRecords());
+    } 
+}
